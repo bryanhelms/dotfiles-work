82a30db8dbecac3930ff984284d9cfac6fee5134
Bryan Helms <bryanhelms@github.com>
2024-04-02 13:02:13 -0700

    Check for presence of local git config

--- scripts/create-local-git-config.sh
+++ scripts/create-local-git-config.sh
@@ -1,12 +1,25 @@
 #!/usr/bin/env bash
 
-# TODO This should check for the presence of a local config file first before creating one
-# TODO Maybe move this into a callable function in here?
-read -p "Enter your git username: " username
-read -p "Enter your git email: " email
+LOCAL_CONFIG_PATH=~/.config/git/config.local
 
-if [ ! -d ~/.config/git ]; then
-    mkdir -p ~/.config/git
+make_config() {
+    read -p "Enter your git name: " username
+    read -p "Enter your git email: " email
+
+    if [ ! -d ~/.config/git ]; then
+        mkdir -p ~/.config/git
+    fi
+
+    printf "[user]\n\tname = %s\n\temail = %s\n" "$username" "$email" >| $LOCAL_CONFIG_PATH
+}
+
+if [ -f $LOCAL_CONFIG_PATH ]; then
+    echo "File exixts."
+    read -p "Local git config already exists. Do you want to replace it? (y/n): " -n 1 -r
+    echo # Add a new line
+    if [[ $REPLY =~ ^[Yy]$ ]]; then
+        make_config
+    fi
+else
+    make_config
 fi
-
-printf "[user]\n\tname = %s\n\temail = %s\n" "$username" "$email" >> ~/.config/git/config.local
