332092ef143435873f97623878a3ed146d8a0c1e
Bryan Helms <bryanhelms@github.com>
2024-04-01 18:28:47 -0700

    Add brew to path

--- install.sh
+++ install.sh
@@ -4,6 +4,9 @@ which -s brew
 if [[ $? != 0 ]] ; then
     # Install Homebrew
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+    (echo; echo 'eval "$(/opt/homebrew/bin/brew shellenv)"') >> ~/.zprofile
+    eval "$(/opt/homebrew/bin/brew shellenv)"
 else
     echo "Homebrew already installed"
 fi
